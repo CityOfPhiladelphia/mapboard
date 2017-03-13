6d068b8b141eac103fb09038da875f65a2b9902e
Robert Martin <rmartin@rmart.in>
2017-03-13 17:38:10 -0400

    Test map by loading OSM basemap

--- src/leaflet/Map.vue
+++ src/leaflet/Map.vue
@@ -1,15 +1,27 @@
 <template>
-  <div id="map">I'm the map</div>
+  <div id="map"></div>
 </template>
 
 <script>
+  const L = require('leaflet');
+
   export default {
-    created() {
-      // create a leaflet obj and mount to map div
+    mounted() {
+      // create leaflet map
+      const map = this.$map = L.map('map');
+
+      // TEST
+      map.setView([51.505, -0.09], 13);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
     }
   };
 </script>
 
 <style>
-
+  #map {
+    height: 100%;
+    weight: 100%;
+  }
 </style>
