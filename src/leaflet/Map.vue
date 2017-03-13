f92c539f4851256037301500b1fa04bc4fa28019
Robert Martin <rmartin@rmart.in>
2017-03-13 17:14:28 -0400

    Stub out Map.vue

---
+++ src/leaflet/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="map">I'm the map</div>
+</template>
+
+<script>
+  export default {
+    created() {
+      // create a leaflet obj and mount to map div
+    }
+  };
+</script>
+
+<style>
+
+</style>
