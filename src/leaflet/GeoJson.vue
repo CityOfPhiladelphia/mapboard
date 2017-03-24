738f38d4e75bc931612ba767437ddaa1360fe77b
andy.rothwell <rothwell.andy@gmail.com>
2017-03-24 16:07:31 -0400

    Create Leaflet GeoJson component

---
+++ src/leaflet/GeoJson.vue
@@ -0,0 +1,43 @@
+<script>
+  import { geoJSON } from 'leaflet';
+  // pascal case
+  const GeoJson = geoJSON;
+
+  export default {
+    props: [
+      'geojson',
+      'color',
+      'weight'
+    ],
+    mounted() {
+      const leafletElement = this.$leafletElement = this.createLeafletElement();
+      const map = this.$store.state.map;
+      // REVIEW kind of hacky/not reactive?
+      if (map) {
+        leafletElement.addTo(map);
+      }
+    },
+    destroyed() {
+      this.$leafletElement._map.removeLayer(this.$leafletElement);
+    },
+    // we don't actually render anything, but need to define either a template
+    // or a render function
+    render(h) {
+      return;
+    },
+    methods: {
+      createLeafletElement() {
+        return new GeoJson(this.$props.geojson, {
+          color: this.$props.color,
+          weight: this.$props.weight
+        });
+      },
+      parentMounted(parent) {
+        const map = parent.$leafletElement;
+        this.$leafletElement.addTo(map);
+      }
+    }
+  };
+
+
+</script>
