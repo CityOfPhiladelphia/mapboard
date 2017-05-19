20a38edd71cd9af66e416b0fe0c81e92a9b547a9
Robert Martin <rmartin@rmart.in>
2017-05-19 16:29:20 -0400

    Render TabGroup content

--- src/components/TopicComponentGroup.vue
+++ src/components/TopicComponentGroup.vue
@@ -1,42 +1,53 @@
 <template>
   <div>
-    <component v-for="(topicComp, topicCompIndex) in components"
-               :is="topicComp.type"
+    <component v-for="(comp, compIndex) in topicComponents"
+               :is="comp.type"
                class="topic-component"
-               :slots="topicComp.slots"
-               :options="topicComp.options"
-               :key="`topic-component-${this.$key}-${topicCompIndex}`"
+               :slots="comp.slots"
+               :options="comp.options"
+               :item="item"
+               :key="getCompKey(key, compIndex)"
     />
   </div>
 </template>
 
 <script>
   import Badge from './topic-components/Badge';
-  import HorizontalTable from './topic-components/HorizontalTable';
-  import VerticalTable from './topic-components/VerticalTable';
   import Callout from './topic-components/Callout';
-  import Image_ from './topic-components/Image';
   import CollectionSummary from './topic-components/CollectionSummary';
-  import TabGroup from './topic-components/TabGroup';
+  import HorizontalTable from './topic-components/HorizontalTable';
+  import Image_ from './topic-components/Image';
+  import VerticalTable from './topic-components/VerticalTable';
 
-   export default {
-     props: ['components'],
-     components: {
-       Badge,
-       HorizontalTable,
-       VerticalTable,
-       Callout,
-       Image_,
-       CollectionSummary,
-       TabGroup,
-     },
-     created() {
-       // generate a (basically) unique id for the group. the go-to npm packages
-       // for uuid generation aren't available as umd builds on unpkg and
-       // therefore won't work with the examples. this is good enough :)
-       this.$key = Math.random().toString(36).substring(7);
-     }
-   };
+  export default {
+    props: ['topicComponents', 'item'],
+    components: {
+      Badge,
+      Callout,
+      CollectionSummary,
+      HorizontalTable,
+      Image_,
+      VerticalTable,
+    },
+    beforeCreate() {
+      // TabGroup imports TopicComponentGroup, which causes a circular
+      // reference issue in webpack. so register the component here.
+      this.$options.components.TabGroup = require('./topic-components/TabGroup.vue');
+    },
+    data() {
+      return {
+        // generate a (basically) unique id for the group. the go-to npm packages
+        // for uuid generation aren't available as umd builds on unpkg and
+        // therefore won't work with the examples. this is good enough :)
+        key: Math.random().toString(36).substring(7)
+      };
+    },
+    methods: {
+      getCompKey(compGroupKey, compIndex) {
+        return `topic-comp-${compGroupKey}-${compIndex}`;;
+      }
+    }
+  };
 </script>
 
 <style scoped>
