79962bca2625153353e56f8356361ec7ad510ec3
Robert Martin <rmartin@rmart.in>
2017-05-24 13:30:57 -0400

    Deploy to NPM on tag builds

--- .travis/s3_utils.sh
+++ .travis/s3_utils.sh
@@ -3,7 +3,10 @@
 # this returns the s3 bucket name for a git branch
 get_s3_bucket() {
   local S3_BUCKET="mapboard"
-  if [ $TRAVIS_BRANCH != "master" ]; then
+  # if this isn't a tag build and the branch is something other than master...
+  # (tag builds always get pushed to master, and non-master branches get pushed
+  # to their own bucket)
+  if [ -z "$TRAVIS_TAG" ] && [ $TRAVIS_BRANCH != "master" ]; then
     S3_BUCKET+="-$TRAVIS_BRANCH"
   fi
   echo $S3_BUCKET
