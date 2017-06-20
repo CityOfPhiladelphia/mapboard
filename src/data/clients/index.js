9ef3739c5267fb837c6bff150306a53e88543bf6
Robert Martin <rmartin@rmart.in>
2017-06-20 12:00:53 -0400

    Refactor data clients

---
+++ src/data/clients/index.js
@@ -0,0 +1,13 @@
+import GeocodeClient from './geocode-client';
+import PwdParcelClient from './pwd-parcel-client';
+import DorParcelClient from './dor-parcel-client';
+import HttpClient from './http-client';
+import EsriClient from './esri-client';
+
+export {
+  GeocodeClient,
+  PwdParcelClient,
+  DorParcelClient,
+  HttpClient,
+  EsriClient
+};
