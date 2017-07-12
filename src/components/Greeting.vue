06110177956b2550b6825a9f76c06c503c67715c
Robert Martin <rmartin@rmart.in>
2017-07-12 16:01:57 -0400

    Show a "not found" message for geocode errors

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="mb-panel-topics-greeting">
     <div class="columns medium-18 medium-centered">
-      <div v-if="!components" class="callout">
+      <div v-if="!components && !hasError" class="callout">
         <p>To start your search, type an address into the search box or click anywhere on the map.</p>
+        <!-- <p v-if="hasError" v-html="message"></p> -->
+      </div>
+
+      <div v-if="!components && hasError" class="callout callout-error" v-html="errorMessage">
+
       </div>
       <component v-if="components"
                  v-for="(topicComp, topicCompIndex) in components"
@@ -31,6 +36,31 @@
       components() {
         const greetingConfig = this.$config.greeting || {};
         return greetingConfig.components;
+      },
+      hasError() {
+        return this.$store.state.geocode.status === 'error';
+      },
+      errorMessage() {
+        const input = this.$store.state.geocode.input;
+        return `
+          <p>
+            We couldn't find <strong>${input}</strong>. Are you sure everything was spelled correctly?
+          </p>
+          <p>
+            Here are some examples of things you can search for:
+          </p>
+          <ul>
+            <li>1234 Market St</li>
+            <li>1001 Pine Street #201</li>
+            <li>12th & Market</li>
+          </ul>
+        `;
+      },
+      calloutClass() {
+        return {
+          'callout': true,
+          'callout-error': this.hasError
+        };
       }
     }
   };
@@ -39,5 +69,20 @@
 <style scope>
   .mb-panel-topics-greeting {
     padding-top: 20px;
+  }
+  .callout {
+    font-size: 20px;
+    /*vertical-align: middle;*/
+  }
+  .callout p {
+    color: #444;
+  }
+  .callout-error {
+    border-left-color: #ff0000;
+    /*background: #ffc4c4;*/
+  }
+  .callout ul {
+    /*font-size: 20px;*/
+    /*color: #222;*/
   }
 </style>
