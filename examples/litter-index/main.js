77caee43a6fb4ca7620419a34681c4d5630841f6
Robert Martin <rmartin@rmart.in>
2017-07-12 16:33:43 -0400

    [Litter Index] update config

--- examples/litter-index/main.js
+++ examples/litter-index/main.js
@@ -10,7 +10,7 @@ Mapboard.default({
   pictometry: {
     enabled: false
   },
-  baseConfig: '//gist.githubusercontent.com/rbrtmrtn/09b4f35396f97499c3097e2fecaed8e7/raw/3c068090d544f3b6e0e31a37acea652a30621c7e/config.js',
+  baseConfig: '//raw.githubusercontent.com/rbrtmrtn/mapboard-base-config/develop/config.js',
   // dataSources: {},
   greeting: {
     components: [
@@ -29,14 +29,15 @@ Mapboard.default({
       label: 'Litter',
       icon: 'fa-trash-o',
       components: [
-        {
-          type: 'badge',
-          slots: {
-            title: 'Litter Index',
-            value: 4.3,
-            description: 'out of 10',
-          }
-        },
+        // this is not ready for the first release of the litter index site
+        // {
+        //   type: 'badge',
+        //   slots: {
+        //     title: 'Litter Index',
+        //     value: 4.3,
+        //     description: 'out of 10',
+        //   }
+        // },
         {
           type: 'vertical-table',
           slots: {
@@ -93,125 +94,14 @@ Mapboard.default({
     }
   ],
   map: {
+    // REVIEW are these necessary?
     center: [39.951618, -75.1650911],
     zoom: 13,
-    basemaps: {
-      pwd: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer',
-        tiledLayers: [
-          'cityBasemapLabels'
-        ],
-        type: 'featuremap'
-      },
-      dor: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/DORBasemap/MapServer',
-        tiledLayers: [
-          'dorBasemapLabels'
-        ],
-        type: 'featuremap'
-      },
-      imagery2016: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2016_3in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2016
-      },
-      imagery2015: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2015_3in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2015
-      },
-      imagery2012: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2012_3in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2012
-      },
-      imagery2010: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2010_3in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2010
-      },
-      imagery2008: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2008_3in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2008
-      },
-      imagery2004: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2004_6in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 2004
-      },
-      imagery1996: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_1996_6in/MapServer',
-        tiledLayers: [
-          'imageryBasemapLabels'
-        ],
-        type: 'imagery',
-        year: 1996
-      }
+    imagery: {
+      enabled: false
     },
-    tiledLayers: {
-      cityBasemapLabels: {
-        // type: 'labels',
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Labels/MapServer'
-      },
-      dorBasemapLabels: {
-        // type: 'labels',
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/DORBasemap_Labels_Test2/MapServer'
-      },
-      imageryBasemapLabels: {
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_Labels/MapServer'
-      }
+    historicBasemaps: {
+      enabled: false
     },
-    dynamicMapLayers: {
-      stormwater: {
-        url: '//gis.phila.gov/arcgis/rest/services/Water/pv_data/MapServer'
-      }
-    },
-    featureLayers: {
-      dorParcels: {
-        url: '//gis.phila.gov/arcgis/rest/services/DOR_ParcelExplorer/rtt_basemap/MapServer/24'
-      },
-      pwdParcels: {
-        url: '//gis.phila.gov/arcgis/rest/services/Water/pv_data/MapServer/0',
-      }
-    }
-  },
-  geocoder: {
-    methods: {
-      search: {
-        url: function (input) {
-          return '//api.phila.gov/ais/v1/search/' + input;
-        },
-        params: {
-          gatekeeperKey: GATEKEEPER_KEY
-        }
-      },
-      reverseGeocode: {
-        url: function (input) {
-          return '//api.phila.gov/ais/v1/reverse_geocode/' + input;
-        },
-        params: {
-          gatekeeperKey: GATEKEEPER_KEY
-        }
-      }
-    }
-  },
+  }
 });
