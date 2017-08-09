0404814170c32bdef8cf10ee3fc6faace57c73ba
andy.rothwell <rothwell.andy@gmail.com>
2017-08-09 14:32:40 -0400

    Remove unnecessary imports from HorizontalTable

--- src/components/topic-components/HorizontalTable.vue
+++ src/components/topic-components/HorizontalTable.vue
@@ -73,8 +73,6 @@
 </template>
 
 <script>
-  import moment from 'moment';
-  import generateUniqueId from '../../util/uniqueId';
   import TopicComponent from './TopicComponent';
   import HorizontalTableRow from './HorizontalTableRow';
 
