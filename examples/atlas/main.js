13262acf2a449536c1cd6b7fe214a61150bad774
andy.rothwell <rothwell.andy@gmail.com>
2017-09-20 16:04:33 -0400

    adds datasource "condoList" as a needed dataSource for the Assessments topic, adds a table group to the Assessments topic with the option "alternate"
    "alternate" contains 2 objects: "mainTable" which is set to the original opa table, and "dependentTable" which contains the info about condo units, which will replace the main table if the main table has no data and the dependent table does have data
    slots on the tables are handled by following commits

--- examples/atlas/main.js
+++ examples/atlas/main.js
@@ -655,7 +655,7 @@ Mapboard.default({
       icon: 'map-marker',
       label: 'Assessments',
       // REVIEW can these be calculated from vue deps?
-      dataSources: ['opa'],
+      dataSources: ['opa', 'condoList'],
       components: [
         {
           type: 'callout',
@@ -664,73 +664,161 @@ Mapboard.default({
           }
         },
         {
-          type: 'vertical-table',
-          slots: {
-            fields: [
-              {
-                label: 'OPA Account #',
-                value: function(state) {
-                  return state.geocode.data.properties.opa_account_num;
-                }
-              },
-              {
-                label: 'OPA Address',
-                value: function(state) {
-                  return state.geocode.data.properties.opa_address;
-                }
-              },
-              {
-                label: 'Owners',
-                value: function(state) {
-                  var owners = state.geocode.data.properties.opa_owners;
-                  var ownersJoined = owners.join(', ');
-                  return ownersJoined;
-                }
-              },
-              {
-                label: 'Assessed Value ' + new Date().getFullYear(),
-                value: function(state) {
-                  var data = state.sources.opa.data;
-                  return data.market_value;
-                },
-                transforms: [
-                  'currency'
-                ]
-              },
-              {
-                label: 'Sale Date',
-                value: function(state) {
-                  var data = state.sources.opa.data;
-
-                  return data.sale_date;
-                },
-                transforms: [
-                  'date'
-                ]
-              },
-              {
-                label: 'Sale Price',
-                value: function(state) {
-                  var data = state.sources.opa.data;
-                  return data.sale_price;
-                },
-                transforms: [
-                  'currency'
-                ]
-              },
-            ],
-          },
+          type: 'table-group',
           options: {
-            externalLink: {
-              action: function(count) {
-                return 'See more';
+            alternate: {
+              mainTable: {
+                dataSource: 'opa',
+                id:'opaData',
               },
-              name: 'Property Search',
-              href: function(state) {
-                var id = state.geocode.data.properties.opa_account_num;
-                return '//property.phila.gov/?p=' + id;
+              dependentTable: {
+                dataSource: 'condoList',
+                id: 'condoList',
               }
-            }
+            },
+            components: [
+
+              {
+                type: 'horizontal-table',
+                options: {
+                  topicKey: 'opa',
+                  id: 'condoList',
+                  useApiCount: true,
+                  // limit: 100,
+                  fields: [
+                    {
+                      label: 'OPA Account',
+                      value: function(state, item) {
+                        var url = window.location.origin + window.location.pathname + '#/' + item.properties.opa_account_num + '/opa'
+                        return "<a href="+url+">"+item.properties.opa_account_num+" <i class='fa fa-external-link'></i></a>";
+                      },
+                    },
+                    {
+                      label: 'Address',
+                      value: function(state, item) {
+                        var url = window.location.origin + window.location.pathname + '#/' + item.properties.opa_account_num + '/opa'
+                        return "<a href="+url+">"+item.properties.street_address+" <i class='fa fa-external-link'></i></a>";
+                      },
+                    },
+                  ], // end fields
+                  // sort: {
+                  //   // this should return the val to sort on
+                  //   getValue: function(item) {
+                  //     // return item.attributes.RECORDING_DATE;
+                  //     return item.attributes.DOCUMENT_DATE;
+                  //   },
+                  //   // asc or desc
+                  //   order: 'desc'
+                  // }
+                },
+                slots: {
+                  title: 'Condominiums',
+                  highestPageRetrieved: function(state) { return state.sources['condoList'].data.page },
+                  pageCount: function(state) { return state.sources['condoList'].data.page_count },
+                  totalSize: function(state) { return state.sources['condoList'].data.total_size },
+                  items: function(state) {
+                    var data = state.sources['condoList'].data.features;
+                    var rows = data.map(function(row){
+                      var itemRow = row;
+                      return itemRow;
+                    });
+                    return rows;
+                  },
+                } // end slots
+              },
+              {
+                type: 'vertical-table',
+                slots: {
+                  fields: [
+                    {
+                      label: 'OPA Account #',
+                      value: function(state) {
+                        return state.geocode.data.properties.opa_account_num;
+                      }
+                    },
+                    {
+                      label: 'OPA Address',
+                      value: function(state) {
+                        return state.geocode.data.properties.opa_address;
+                      }
+                    },
+                    {
+                      label: 'Owners',
+                      value: function(state) {
+                        var owners = state.geocode.data.properties.opa_owners;
+                        var ownersJoined = owners.join(', ');
+                        return ownersJoined;
+                      }
+                    },
+                    {
+                      label: 'Assessed Value ' + new Date().getFullYear(),
+                      value: function(state) {
+                        var data = state.sources.opa.data;
+                        // return data.market_value;
+                        var result;
+                        if (data) {
+                          result = data.market_value;
+                        } else {
+                          result = 'no data';
+                        }
+                        return result;
+                      },
+                      transforms: [
+                        'currency'
+                      ]
+                    },
+                    {
+                      label: 'Sale Date',
+                      value: function(state) {
+                        var data = state.sources.opa.data;
+                        // return data.sale_date;
+                        var result;
+                        if (data) {
+                          result = data.sale_date;
+                        } else {
+                          result = 'no data';
+                        }
+                        return result;
+                      },
+                      transforms: [
+                        'date'
+                      ]
+                    },
+                    {
+                      label: 'Sale Price',
+                      value: function(state) {
+                        var data = state.sources.opa.data;
+                        // return data.sale_price;
+                        var result;
+                        if (data) {
+                          result = data.sale_price;
+                        } else {
+                          result = 'no data';
+                        }
+                        return result;
+                      },
+                      transforms: [
+                        'currency'
+                      ]
+                    },
+                  ],
+                },
+                options: {
+                  id: 'opaData',
+                  externalLink: {
+                    action: function(count) {
+                      return 'See more';
+                    },
+                    name: 'Property Search',
+                    href: function(state) {
+                      var id = state.geocode.data.properties.opa_account_num;
+                      return '//property.phila.gov/?p=' + id;
+                    }
+                  }
+                }
+              }
+
+            ]
           }
         }
       ],
