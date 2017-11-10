b1658152e0fd00d999c20eab2caa088dc066c4ec
Robert Martin <rmartin@rmart.in>
2017-11-10 12:07:06 -0500

    [atlas] Update initial greeting

--- examples/atlas/main.js
+++ examples/atlas/main.js
@@ -779,10 +779,20 @@ Mapboard.default({
     }
   },
   greeting:{
-    initialMessage: "Explore permits, zoning, deeds and other related City data by searching on an address.<br><br> \
-    See results mapped against present and past aerial and street-view imagery or using historical maps of Philadelphia.<br><br> \
-    To start your search, type an address, street intersection, tax account number, or deed Map Registry number into the search box – or – click anywhere on the map.",
-
+    initialMessage: '\
+      <h2>Atlas is your front door to the City of Philadelphia.</h2>\
+      <p>Here are some things you can do with Atlas:</p>\
+      <div class="callout">\
+        <ul>\
+          <li>Get the history of permits, licenses, and inspections at any address</li>\
+          <li>Research real estate information including property values, zoning, and document archives</li>\
+          <li>Get easy access to a variety of hard-to-find City resources</li>\
+          <li>View recent activity around your address, such as crimes, 311 service requests, and more</li>\
+          <li>Explore historical imagery and maps</li>\
+        </ul>\
+      </div>\
+      <p>To get started, click anywhere on the map, or type an address, intersection, property assessment account number, or Department of Records Map Registry number into the search box.</p>\
+    ',
   },
   topics: [
     {
