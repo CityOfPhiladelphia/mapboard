3f1aee2f78f83b20c3f2a7d631c6f40a79844fd2
andy.rothwell <rothwell.andy@gmail.com>
2018-02-14 17:05:32 -0500

    changes test.js

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -5,11 +5,34 @@ function getAtlasExampleUrl(browser) {
   return `${devServerUrl}/examples/atlas`;
 }
 
+// partial function to bootstrap an atlas test
+// function testAtlas(browser, callback) {
+//   browser
+//     .url(getAtlasExampleUrl(browser))
+//     .waitForElementVisible('#mb-root', 5000);
+//
+//   callback()
+//
+//   end();''
+// }
+
 module.exports = {
-  'Atlas - Basic Test' : function (browser) {
+  // TODO replace this with more meaningful tests
+  '[Atlas] Basic Test': browser => {
     browser
       .url(getAtlasExampleUrl(browser))
-      .assert.title('Atlas | phila.gov')
+      .waitForElementVisible('#mb-root', 5000);
+      .setValue('.mb-search-control-input', '1913 green st')
+      .click('.mb-search-control-button')
+
+      // .assert.containsText('.greeting > h2', 'front door')
+
       .end();
+  },
+
+  // test to make sure an address with multiple dor parcels is rendering
+  // correctly
+  '[Atlas] Multiple Parcels': browser => {
+    
   },
 };
