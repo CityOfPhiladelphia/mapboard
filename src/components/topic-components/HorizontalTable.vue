f710b96d76eea9cce4c6c580a92686f39ce0842f
andy.rothwell <rothwell.andy@gmail.com>
2018-04-05 15:52:41 -0400

    adds computed called "shouldShowHeaders" to HorizontalTable.vue, so that you can control whether a horiz table has headers

--- src/components/topic-components/HorizontalTable.vue
+++ src/components/topic-components/HorizontalTable.vue
@@ -6,7 +6,9 @@
     </button> -->
     <div v-if="shouldShowTable">
       <!-- controls -->
-      <div class="mb-horizontal-table-controls">
+      <div class="mb-horizontal-table-controls"
+           v-if="shouldShowHeaders"
+      >
         <div v-if="!!options.filters"
              class="vertically-centered"
         >
@@ -74,7 +76,7 @@
         </div>
       </div> <!-- end of mb-horizontal-table-controls block -->
 
-      <div class="mb-horizontal-table-body">
+      <div :class="{ 'mb-horizontal-table-body': true, 'no-padding': !shouldShowHeaders }">
         <div v-if="slots.title">
           <h4 style="display:inline-block">
             {{ evaluateSlot(slots.title) }} {{ countText }}
@@ -91,7 +93,7 @@
         </div>
 
         <table role="grid" class="stack">
-          <thead>
+          <thead v-if="shouldShowHeaders !== false">
             <tr>
               <th v-for="field in fields">{{ evaluateSlot(field.label) }}</th>
             </tr>
@@ -108,9 +110,10 @@
         </table>
 
         <!-- external link (aka "see more")-->
-        <div class="external-link">
-          <a v-if="options.externalLink && shouldShowExternalLink"
-             :href="externalLinkHref"
+        <div class="external-link"
+             v-if="options.externalLink && shouldShowExternalLink"
+        >
+          <a :href="externalLinkHref"
              class="external"
              target="_blank"
           >
@@ -211,6 +214,13 @@
       }
     },
     computed: {
+      shouldShowHeaders() {
+        if (typeof this.options.shouldShowHeaders === 'undefined') {
+          return true;
+        } else {
+          return this.options.shouldShowHeaders;
+        }
+      },
       shouldShowDownloadButton() {
         let downloadButton = false;
         if (this.options.downloadButton) {
@@ -579,6 +589,7 @@
         this.searchText = "";
       },
       filterItems(items, filters, filterSelections) {
+        // console.log('typeof items:', typeof items);
         // console.log('FILTER ITEMS is running, items:', items, 'filters:', filters, 'filterSelections:', filterSelections);
         let itemsFiltered = items.slice();
 
@@ -807,6 +818,12 @@
 
   .mb-horizontal-table-body {
     padding-top: 1rem;
+    padding-bottom: 0.35rem;
+  }
+
+  .no-padding {
+    padding-top: 0;
+    padding-bottom: 0;
   }
 
   .center-button {
